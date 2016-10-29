de44e16fa6a433e9f0854e0962a3277a39e31591
Adam Oswalt <email@adamoswalt.com>
2016-10-28 20:50:11 -0500

    more flexible drawBars and demoing with 5 sets

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const ctx = document.getElementById('noise1d').getContext('2d')
-ctx.fillStyle = 'green'
+const barCount = 25
+const barColor = 'green'
 
-const drawBars = list => {
+//NOTE(adam): draw the list of values as bars on the 2d context
+const drawBars = (ctx, list) => {
   const barCount = list.length
   const barWidth = ctx.canvas.width / barCount
   const maxHeight = ctx.canvas.height
+  const barMaxHeight = maxHeight * .8
 
   list.forEach((val, index, list) => {
     //NOTE(adam): ensure val 0 to 1
@@ -12,12 +14,17 @@ const drawBars = list => {
       console.err('number outside range', val)
     }
 
-    const height = val * maxHeight
+        const height = val * barMaxHeight
     ctx.fillRect(barWidth * index, maxHeight - height, barWidth, height)
   })
 }
 
-const nums = []
-for(let i = 0; i < 10; ++i) { nums[i] = Math.random() }
-drawBars(nums)
+//NOTE(adam): get an array of 2d contexts from the nodelist of canvases
+const ctx1dList = Array.from(document.querySelectorAll('.canvas1d')).map((c) => c.getContext('2d'))
+ctx1dList.forEach(ctx => ctx.fillStyle = barColor)
 
+ctx1dList.forEach((ctx, i) => {
+  const nums = []
+  for(let i = 0; i < barCount; ++i) { nums[i] = Math.random() }
+  drawBars(ctx, nums)
+})
