cc79d94f151899c58a67385530520b63ee361c4e
Adam Oswalt <email@adamoswalt.com>
2016-11-02 00:29:41 -0500

    makeNoise function with freq and amp paremeters

--- script.js
+++ script.js
@@ -53,16 +53,21 @@ const randomInt = (min, max, randomFunc) => {
 //NOTE(adam): average adjacent values for smoother noise output
 const adjacentMin = (noise) => noise.map((e, i) => i === 0 ? noise[i] : Math.min(noise[i - 1], noise[i]))
 
+const makeNoise = (freqFunc = (x) => 1, ampFunc = (x) => 1) =>
+  (x) => Math.cos(x * freqFunc(x)) * ampFunc(x)
+
 
 //NOTE(adam): get an array of 2d contexts from the nodelist of canvases
 const ctx1dList = Array.from(document.querySelectorAll('.canvas1d')).map((c) => c.getContext('2d'))
-ctx1dList.forEach(ctx => ctx.fillStyle = barColor)
-
+ctx1dList.forEach((ctx) => ctx.fillStyle = barColor)
 
 ctx1dList.forEach((ctx, ctxIndex) => {
-  const freq = 0.1 * (ctxIndex + 1)
+  const freq = (i) => 0.1 * (ctxIndex + 1)
+  const noiseFunc = makeNoise(freq)
+
   const noise = []
-  const random = makeRandom(ctxIndex)
-  for(let i = 0; i < barCount; ++i) { noise[i] = Math.cos(i * freq) }
-  drawBars(ctx, normalize(noise))
+  for(let i = 0; i < barCount; ++i) {
+    noise[i] = noiseFunc(i)
+  }
+  drawBars(ctx, noise)
 })
