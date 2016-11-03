ff4eb5d3a79706131946c0cd57e20d7d767e5a53
Adam Oswalt <email@adamoswalt.com>
2016-11-02 22:25:39 -0500

    offset noise function using sine

--- js/noise.js
+++ js/noise.js
@@ -3,6 +3,10 @@ var Noise = ((noise) => {
   noise.makeNoise = (freqFunc = (x) => 1, ampFunc = (x) => 1) =>
     (x) => Math.cos(ampFunc(x) + x * freqFunc(x))
 
+  //NOTE(adam): create sine noise function with freq and amp functions
+  noise.makeNoiseOffset = (freqFunc = (x) => 1, ampFunc = (x) => 1) =>
+    (x) => Math.sin(ampFunc(x) + x * freqFunc(x))
+
   //NOTE(adam): average adjacent values for smoother noise output
   noise.adjacentMin = (noise) =>
     noise.map((e, i) => i === 0 ? noise[i] : Math.min(noise[i - 1], noise[i]))
